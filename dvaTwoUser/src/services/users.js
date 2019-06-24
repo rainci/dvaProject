5a4858cc00c48d462cea8269ce8a6856c116a841
univer <univer@univerdeiMac-2.local>
2019-06-24 14:10:47 +0800

    add new pro

--- dvaTwoUser/src/services/users.js
+++ dvaTwoUser/src/services/users.js
@@ -1,10 +1,10 @@
 import request from '../utils/request';
-
+const api = '/api'
 export function fetch({ page = 1 }) {
-  return request(`http://localhost:8000/api/users?_page=${page}&_limit=5`);
+  return request(`${api}/users?_page=${page}&_limit=5`);
 }
 export function remove(id) {
-  return request(`/api/users/${id}`, {
+  return request(`${api}/users/${id}`, {
     method: 'DELETE',
   });
 }
