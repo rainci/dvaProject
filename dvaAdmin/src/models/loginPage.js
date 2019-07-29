13a69f8e99c067fd5164954df04c5ff82f40c235
rainci <2805242676@qq.com>
2019-07-29 23:19:33 +0800

    add 异步错误提示

--- dvaAdmin/src/models/loginPage.js
+++ dvaAdmin/src/models/loginPage.js
@@ -1,27 +1,36 @@
 import * as server from '../services/server';
+import { message } from 'antd';
+
 export default {
-    namespace: 'loginPage',
-    state: {},
-    reducers: {
-        login(state,{payload: { data }={} }){
-            return {
-                ...state,
-                ...data,
-            }
-        }
-    },
-    effects: {
-        *fetchLogin({ payload }, { call, put }){
-            const { userName, password } = payload || {};  
-            const result = yield call(server.loginFn, {userName,password})//call异步
-            console.log('result:',result)
-            if(result.data && result.data.code === 200){
-                yield put({type:'login',payload:result.data})//同步
-            }
-        }
-    },
-    subscriptions: {
-        setup({ dispatch, history }) {  // eslint-disable-line
-        },
-    },
+	namespace: 'loginPage',
+	state: {},
+	reducers: {
+		login(state, { payload: { data } = {} }) {
+			return {
+				...state,
+				...data,
+			}
+		}
+	},
+	effects: {
+		*fetchLogin({ payload }, { call, put }) {
+			try {
+				const { userName, password } = payload;
+				const result = yield call(server.loginFn, { userName, password })//call异步
+				const isSuccess = result.data && result.data.code === 200;
+				if (isSuccess) {
+					yield put({ type: 'login', payload: result.data })//同步
+				} else {
+					message.warn(result.message);
+				}
+				return isSuccess;
+			} catch (error) {
+				message.warn('jiekou');
+			}
+		}
+	},
+	subscriptions: {
+		setup({ dispatch, history }) {  // eslint-disable-line
+		},
+	},
 }
--- dvaAdmin/src/routes/login/index.js
+++ dvaAdmin/src/routes/login/index.js
@@ -9,103 +9,106 @@ import { setList } from '../../utils'
 const FormItem = Form.Item;
 
 class LoginPage extends React.PureComponent {
-    state={
+	state = {
 
-    }
-    /***********公共方法 begin *****************/
-    setStateValueFn = (key, value) => {//设置state值
-        this.setState({
-            [key]: value
-        })
-    }
-    /***********公共方法 end *****************/
-    /***************************页面业务逻辑 begin ******************************/
-    loginSubmit = () => {//登录按钮
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                console.log(values)
-                let { userName, password } = values;
-                if (userName === 'xixi' && password === '111111') {
-                    this.props.dispatch({type:'loginPage/fetchLogin',payload: {userName,password}})
-                    .then(()=> {
-                        setList('userInfo', { userName:this.props.userName, token: this.props.token})
-                        this.props.history.push('/main')
-                    })
-                    .catch(err =>  {
-                        message.warn(err)
-                    })
-                    
-                }else{
-                    message.warn('用户名或密码不正确')
-                }
-            }
-        })
+	}
+	/***********公共方法 begin *****************/
+	setStateValueFn = (key, value) => {//设置state值
+		this.setState({
+			[key]: value
+		})
+	}
+	/***********公共方法 end *****************/
+	/***************************页面业务逻辑 begin ******************************/
+	loginSubmit = () => {//登录按钮
+		this.props.form.validateFields((err, values) => {
+			if (!err) {
+				console.log(values)
+				let { userName, password } = values;
+				if (userName === 'xixi' && password === '111111') {
+					this.props.dispatch({
+						type: 'loginPage/fetchLogin',
+						payload: { userName, password }
+					})
+						.then((isSuccess) => {
+							if (!isSuccess) {
+								return;
+							}
+							setList('userInfo', { userName: this.props.userName, token: this.props.token })
+							this.props.history.push('/main')
+						})
 
-    }
-    /***************************页面业务逻辑 end ******************************/
-    /***************************生命周期 begin *******************************/
-    componentDidMount() {
-        
-    }
-    /***************************生命周期 end *******************************/
-    
-    render() {
-        const { getFieldDecorator } = this.props.form;
-        return (
-            <div className={styles.loginBody}>
-                <div className={styles.loginlogo}></div>
-                <div className={styles.textBox}>
-                    <div className={styles.textLine}></div>
-                    <div>
-                        <h3>CMS管理系统</h3>
-                        <p>让视频的每一秒都是人工智能</p>
-                    </div>
-                </div>
-                <div className={styles.loginBox}>
-                    <h2>欢迎登录</h2>
-                    <Form className="login-form">
-                        <FormItem>
-                            {getFieldDecorator('userName', {
-                                validateTrigger: "onBlur",
-                                validateFirst: true,
-                                rules: [{ required: true, message: '请输入帐号!' }],
-                            })(
-                                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-                            )}
-                        </FormItem>
-                        <FormItem>
-                            {getFieldDecorator('password', {
-                                validateTrigger: "onBlur",
-                                validateFirst: true,
-                                rules: [
-                                    { required: true, message: '请输入密码!' },
-                                    { min: 6, max: 20, message: '密码长度限制在8到20位' },
-                                ],
-                            })(
-                                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-                            )}
-                        </FormItem>
+				} else {
+					message.warn('用户名或密码不正确')
+				}
+			}
+		})
 
-                        <FormItem>
-                            <Button type="primary" className="login-form-button" onClick={this.loginSubmit} style={{ width: '100%' }}>
-                                登录
+	}
+	/***************************页面业务逻辑 end ******************************/
+	/***************************生命周期 begin *******************************/
+	componentDidMount() {
+
+	}
+	/***************************生命周期 end *******************************/
+
+	render() {
+		const { getFieldDecorator } = this.props.form;
+		return (
+			<div className={styles.loginBody}>
+				<div className={styles.loginlogo}></div>
+				<div className={styles.textBox}>
+					<div className={styles.textLine}></div>
+					<div>
+						<h3>CMS管理系统</h3>
+						<p>让视频的每一秒都是人工智能</p>
+					</div>
+				</div>
+				<div className={styles.loginBox}>
+					<h2>欢迎登录</h2>
+					<Form className="login-form">
+						<FormItem>
+							{getFieldDecorator('userName', {
+								validateTrigger: "onBlur",
+								validateFirst: true,
+								rules: [{ required: true, message: '请输入帐号!' }],
+							})(
+								<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+							)}
+						</FormItem>
+						<FormItem>
+							{getFieldDecorator('password', {
+								validateTrigger: "onBlur",
+								validateFirst: true,
+								rules: [
+									{ required: true, message: '请输入密码!' },
+									{ min: 6, max: 20, message: '密码长度限制在8到20位' },
+								],
+							})(
+								<Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+							)}
+						</FormItem>
+
+						<FormItem>
+							<Button type="primary" className="login-form-button" onClick={this.loginSubmit} style={{ width: '100%' }}>
+								登录
                             </Button>
-                        </FormItem>
+						</FormItem>
 
-                    </Form>
-                </div>
+					</Form>
+				</div>
 
-            </div>
-        )
-    }
+			</div>
+		)
+	}
 }
 const mapStateToProps = ({ loginPage, loading }) => {
-    const { userName, token } = loginPage;
-    console.log(111,loginPage,userName,token )
-    return {
-        loading,
-        userName,
-        token,
-    };
+	const { userName, token } = loginPage;
+	console.log(111, loginPage, userName, token)
+	return {
+		loading,
+		userName,
+		token,
+	};
 }
 export default connect(mapStateToProps)(Form.create()(LoginPage));
